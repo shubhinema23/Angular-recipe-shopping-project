9aac0601b1f41ae1a6088469d1ed4001e5469fa6
shubhinema23 <81033011+shubhinema23@users.noreply.github.com>
2021-04-07 18:57:24 +0530

    Add files via upload

---
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -0,0 +1,66 @@
+import { Component, OnInit, OnDestroy, ViewChild} from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
+
+import { Ingredient } from '../../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list.service';
+
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping-edit.component.html',
+  styleUrls: ['./shopping-edit.component.css']
+})
+export class ShoppingEditComponent implements OnInit, OnDestroy{
+ @ViewChild('f', { static: false }) slForm: NgForm;
+subscription: Subscription;
+editMode = false;
+editedItemIndex: number;
+editedItem: Ingredient;
+
+  constructor(private slService: ShoppingListService) { }
+
+  ngOnInit(): void {
+    this.subscription = this.slService.startedEditing.subscribe(
+      (index: number) => {
+        this.editedItemIndex = index;
+        this.editMode = true;
+        this.editedItem = this.slService.getEditIngredients(index);
+        this.slForm.setValue({
+          'name': this.editedItem.name,
+          'amount': this.editedItem.amount
+        })
+      }
+    )
+  }
+
+
+
+  onAddIngredient(form: NgForm){
+    const value = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount);
+    if(this.editMode){
+      this.slService.updateIngredient(this.editedItemIndex, newIngredient);
+    }else{
+      this.slService.addIngredient(newIngredient);
+    }
+    this.editMode = false;
+    form.reset();
+	}
+
+  onClearForm(){
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
+  onDelete(){
+    this.slService.deleteIngredient(this.editedItemIndex);
+    this.onClearForm();
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
+
+
+}
